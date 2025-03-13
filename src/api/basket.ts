a5e3d75d1e7b9c4cd4fc6109ae9d4ee3fe5abc9f
galio <yangkun>
2025-03-14 05:38:00 +0800

    添加购物车和订单 API，包含获取、添加、删除和更新功能

--- src/api/basket.ts
+++ src/api/basket.ts
@@ -1,9 +1,31 @@
-// import { request } from '@/utils/request.ts';
-// import { request } from "@/utils/request";
+import request from "@/utils/request.ts";
 
-// export function getBasketList() {
-//   return request({
-//     url: "/basket",
-//     method: "get",
-//   });
-// }
+export function getBasketList() {
+  return request({
+    url: "/basket",
+    method: "get",
+  });
+}
+
+export function addBasket(data) {
+  return request({
+    url: "/basket",
+    method: "post",
+    data,
+  });
+}
+
+export function deleteBasket(id) {
+  return request({
+    url: `/basket/${id}`,
+    method: "delete",
+  });
+}
+
+export function updateBasket(data) {
+  return request({
+    url: `/basket/${data.id}`,
+    method: "put",
+    data,
+  });
+}
---
+++ src/api/order.ts
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export function getOrderList(params: any) {
+  return request({
+    url: '/order/list',
+    method: 'get',
+    params
+  })
+}
+
+export function getOrderDetail(params: any) {
+  return request({
+    url: '/order/detail',
+    method: 'get',
+    params
+  })
+}
+
+export function getOrderStatus(params: any) {
+  return request({
+    url: '/order/status',
+    method: 'get',
+    params
+  })
+}
+
+export function updateOrderStatus(data: any) {
+  return request({
+    url: '/order/updateStatus',
+    method: 'post',
+    data
+  })
+}
