d39512299a58ae3f46735f329e76888e1368daae
galio <yangkun>
2025-03-14 05:50:20 +0800

    更新看板视图，修改标题为“stock kanban”，并添加产品信息表头

--- src/views/kanban/index.vue
+++ src/views/kanban/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>kanban</div>
+  <div>stock kanban</div>
+
+  <div class="product">
+    <div class="product-header">
+      <span>产品</span>
+      <span>类型</span>
+      <span>品牌</span>
+      <span>库存</span>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
