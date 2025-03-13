963540f6a5666005c032eee8db245162cce69ee2
galio <yangkun>
2025-03-13 20:07:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    syndra
+  </h1>
+</template>
+
+<style scoped></style>
